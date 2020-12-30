b3a65ccc35b92b0cc0754cffa67d5d4bca4f6108
jsta <stachel2@msu.edu>
2020-12-30 09:18:44 -0500

    try source activate

--- template_docker-w-conda/docker-w-conda.sh
+++ template_docker-w-conda/docker-w-conda.sh
@@ -6,6 +6,7 @@ set -e
 ENVNAME=py-data-sci
 # if you need the environment directory to be named something other than the environment name, change this line
 ENVDIR=$ENVNAME
+source activate $ENVNAME
 
 # modify this line to run your desired Python script and any other work you need to do
-conda run -n $ENVNAME python3 hello.py
+python3 hello.py

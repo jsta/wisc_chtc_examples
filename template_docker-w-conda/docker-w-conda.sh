732b9c7b806cfeef147b117512c69620a6529235
jsta <stachel2@msu.edu>
2020-12-30 08:58:32 -0500

    no need to unpack conda env tarball

--- template_docker-w-conda/docker-w-conda.sh
+++ template_docker-w-conda/docker-w-conda.sh
@@ -9,8 +9,6 @@ ENVDIR=$ENVNAME
 
 # these lines handle setting up the environment; you shouldn't have to modify them
 export PATH
-mkdir $ENVDIR
-tar -xzf $ENVNAME.tar.gz -C $ENVDIR
 . $ENVDIR/bin/activate
 
 # modify this line to run your desired Python script and any other work you need to do

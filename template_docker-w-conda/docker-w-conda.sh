ecace9bb13b6e91f13a0bc24c1ebc1630e74ff60
jsta <stachel2@msu.edu>
2021-05-11 13:14:51 -0400

    use custom conda env with data

--- template_docker-w-conda/docker-w-conda.sh
+++ template_docker-w-conda/docker-w-conda.sh
@@ -3,7 +3,7 @@
 set -e
 
 # replace env-name on the right hand side of this line with the name of your conda environment
-ENVNAME=py-data-sci
+ENVNAME=template_docker-w-conda
 # if you need the environment directory to be named something other than the environment name, change this line
 ENVDIR=$ENVNAME
 source activate $ENVNAME
--- template_docker-w-conda/hello.py
+++ template_docker-w-conda/hello.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-d = {"col1": [1, 2], "col2": [3, 4]}
+df = pd.read_csv("data.csv")
 
-df = pd.DataFrame(data=d)
+print(df)
 
 df.to_csv("df.csv", index=False)

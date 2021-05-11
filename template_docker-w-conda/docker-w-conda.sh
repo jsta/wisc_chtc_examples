8e395beab456cfce3868e5b2361a3debf7465f06
jsta <stachel2@msu.edu>
2021-05-11 14:13:19 -0400

    use exact code from docs

--- template_docker-w-conda/docker-w-conda.sh
+++ template_docker-w-conda/docker-w-conda.sh
@@ -6,8 +6,11 @@ set -e
 ENVNAME=template_docker-w-conda
 # if you need the environment directory to be named something other than the environment name, change this line
 ENVDIR=$ENVNAME
-conda env list
-source activate $ENVNAME
+
+export PATH
+mkdir $ENVDIR
+tar -xzf $ENVNAME.tar.gz -C $ENVDIR
+. $ENVDIR/bin/activate
 
 # modify this line to run your desired Python script and any other work you need to do
 python3 hello.py
